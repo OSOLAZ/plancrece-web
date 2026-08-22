44abeb07f9050112ea11728a811c1ab5d71ada17
O.S <oscarsolaz@gmail.com>
2026-08-22 11:26:46 +0200

    fix: remove unused TrendingUp import in Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router'
-import { TrendingUp, Mail, MessagesSquare } from 'lucide-react'
+import { Mail, MessagesSquare } from 'lucide-react'
 
 export default function Footer() {
   return (
